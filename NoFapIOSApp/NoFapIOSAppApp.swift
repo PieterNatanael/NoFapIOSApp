8c9bdb9b4e423a0351a4a3097771b773189d0112
PieterNatanael <pieternatanael@icloud.com>
2023-08-31 08:47:34 +0930

    Initial Commit

---
+++ NoFapIOSApp/NoFapIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoFapIOSAppApp.swift
+//  NoFapIOSApp
+//
+//  Created by Pieter Yoshua Natanael on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NoFapIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
